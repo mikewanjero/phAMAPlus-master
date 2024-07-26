afe1dccb25641c506355ba7d12ee515e4d1f80c3
mikewanjero <mikewanj@gmail.com>
2024-07-26 14:41:13 +0300

    Changes to new 'Forget Password' page

--- components/ForgetPassword.js
+++ components/ForgetPassword.js
@@ -1,4 +1,6 @@
 import { React, useState } from "react";
+import { Platform, ToastAndroid } from "react-native";
+import colors from "../config/colors";
 import {
   NativeBaseProvider,
   View,
@@ -19,11 +21,11 @@ import {
   Center,
   KeyboardAvoidingView,
 } from "native-base";
-import { Platform } from "react-native";
-import colors from "../config/colors";
+import { MaterialIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function ForgetPassword() {
-  const [isLoading, setIsLoading] = useState(false); //Loading state of action button (Create Account)
+export default function ForgetPassword({ navigation }) {
+  const [isLoading, setIsLoading] = useState(false); //Loading state of action button (Reset Password)
   const [formData, setFormData] = useState({
     nationalID: "200011233",
     pin: "",
@@ -96,6 +98,18 @@ export default function ForgetPassword() {
     }
   };
 
+  const handleSubmit = () => {
+    validate()
+      ? handleLogin()
+      : ToastAndroid.showWithGravityAndOffset(
+          "Please fill in the blanks!",
+          ToastAndroid.LONG,
+          ToastAndroid.BOTTOM,
+          25,
+          50
+        );
+  };
+
   return (
     <NativeBaseProvider>
       <KeyboardAvoidingView
@@ -114,7 +128,7 @@ export default function ForgetPassword() {
               fontWeight={"bold"}
               color={colors.phAMACoreColor1}
             >
-              phAMACore™
+              phAMACore™ Loyalty
             </Heading>
           </Center>
         </View>
@@ -127,7 +141,7 @@ export default function ForgetPassword() {
               color={colors.phAMACoreColor1}
               _dark={{ color: "orange.50" }}
             >
-              Welcome back!
+              Password Reset
             </Heading>
             <Heading
               size={"xs"}
@@ -140,17 +154,59 @@ export default function ForgetPassword() {
             <VStack space={3} mt={"5"}>
               <FormControl>
                 <FormControl.Label>National ID</FormControl.Label>
-                <Input placeholder="12345678" placeholderTextColor="#707070" />
+                <Input
+                  placeholder="12345678"
+                  placeholderTextColor="#707070"
+                  value={formData.nationalID}
+                  onChangeText={(value) =>
+                    setFormData({ ...formData, nationalID: value })
+                  }
+                  borderWidth={1}
+                  borderColor={"gray.400"}
+                />
               </FormControl>
               <FormControl>
                 <FormControl.Label>Password</FormControl.Label>
-                <Input />
+                <Input
+                  type={show ? "text" : "password"}
+                  keyboardType="numeric"
+                  value={formData.pin}
+                  onChangeText={(value) =>
+                    setFormData({ ...formData, pin: value })
+                  }
+                  borderWidth={1}
+                  borderColor={"gray.400"}
+                  InputRightElement={
+                    <Pressable onPress={() => setShow(!show)}>
+                      <Icon
+                        as={
+                          <MaterialIcons
+                            name={show ? "visibility" : "visibility-off"}
+                          />
+                        }
+                        size={5}
+                        mr={2}
+                        color={"gray.600"}
+                      />
+                    </Pressable>
+                  }
+                />
+                {"pin" in errors ? (
+                  <FormControl.ErrorMessage>
+                    {errors.pin}
+                  </FormControl.ErrorMessage>
+                ) : (
+                  ""
+                )}
               </FormControl>
               <Button
                 mt={"7"}
                 bg={colors.phAMACoreColor3}
                 p={4}
                 _text={{ fontSize: 14, fontWeight: "semibold", color: "white" }}
+                isLoading={isLoading}
+                isLoadingText="Resetting password"
+                onPress={handleSubmit}
               >
                 Reset Password
               </Button>
