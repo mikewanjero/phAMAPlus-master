5789b3e1036239926505c208433ae101d620fb86
Paul Netia <netiapaul23@gmail.com>
2024-08-09 16:29:42 +0300

    - forgot password fix - http use for production build

--- components/ForgetPassword.js
+++ components/ForgetPassword.js
@@ -32,26 +32,21 @@ export default function ForgetPassword({ navigation }) {
     pin: "",
     confirmPin: "",
   }); // State displaying data inputs (creating new user) - key and object
-  const [show, setShow] = useState(false); // State to show or hide input text and visibility
   const [error, setError] = useState(""); // State to display an error when logging in
   const [errors, setErrors] = useState({}); // Display errors in validation in case of missing or wrong inputs
+  const [status, setStatus] = useState({
+    error: "",
+    success: "",
+  });
 
   const validate = () => {
     if (!formData.nationalID && !formData.pin && !formData.confirmPin) {
       setErrors({
         nationalID: "National ID is required",
-        pin: "Pin is required",
-        confirmPin: "Confirm Pin is required",
       });
       return false;
     } else if (!formData.nationalID) {
       setErrors({ nationalID: "National ID is required" });
-      return false;
-    } else if (!formData.pin) {
-      setErrors({ pin: "Pin is required" });
-      return false;
-    } else if (!formData.confirmPin) {
-      setErrors({ confirmPin: "Confirm Pin is required" });
       return false;
     }
     setErrors({ nationalID: "200011233", pin: "", confirmPin: "" });
@@ -72,42 +67,44 @@ export default function ForgetPassword({ navigation }) {
   const handleLogin = async () => {
     setIsLoading(true);
     setError("");
+    setStatus({
+      error: "",
+      success: "",
+    });
+    console.log(formData.nationalID);
     try {
       let response = await fetch(
-        `http://www.phamacoretraining.co.ke:81/CustomerPoints/ForgotPassword`,
+        `http://www.phamacoretraining.co.ke:81/CustomerPoints/ForgotPassword?idNumber=${formData.nationalID}`,
         {
           method: "POST",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({
-            idnumber: formData.nationalID,
-            pin: formData.pin,
-            confirmPin: formData.confirmPin,
-          }),
         }
       );
       if (response.ok) {
         let data = await response.json();
         console.log(data);
         setIsLoading(false);
-
-        await AsyncStorage.setItem("token", data.token);
-        await AsyncStorage.setItem("memberno", data.user.memberno);
-        await AsyncStorage.setItem("fullusername", data.user.fullusername);
-
-        return navigation.navigate("Dashboard");
+        setStatus({
+          error: "",
+          success: `${data?.message} and sent to ${data?.phoneNumber}`,
+        });
+        // return navigation.navigate("Login");
       } else {
         let data = await response.json();
         console.log(data);
         setIsLoading(false);
-        setError(data.errors.message);
+        setStatus({ error: "", success: data.errors?.message });
       }
     } catch (error) {
       console.log(error);
       setIsLoading(false);
-      alert("Error logging in. Check your credentials!");
+      setStatus({
+        error: "An error occured processing your current request!",
+        success: "",
+      });
       throw new Error(error);
     }
   };
@@ -148,7 +145,32 @@ export default function ForgetPassword({ navigation }) {
             </Heading>
           </Center>
         </View>
-
+        {error && (
+          <Alert w="100%" status="error">
+            <Text
+              fontSize="md"
+              fontWeight="medium"
+              _dark={{
+                color: "coolGray.800",
+              }}
+            >
+              {error}
+            </Text>
+          </Alert>
+        )}
+        {status.success && (
+          <Alert w="100%" status="success">
+            <Text
+              fontSize="md"
+              fontWeight="medium"
+              _dark={{
+                color: "coolGray.800",
+              }}
+            >
+              {status.success}
+            </Text>
+          </Alert>
+        )}
         <Center px={5} flex={1} bgColor={"coolGray.200"}>
           <Box p={"2"} w={"100%"} flex={1}>
             <Heading
